57f1dac17c7537939a7413fa5db12a19a8ada172
Urena <lurenas@gmail.com>
2022-11-24 15:23:13 -0600

    End of section 2

---
+++ NZWalks/NZWalks.API/Migrations/20221123230543_Second.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nmae",
+                table: "Regions",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Regions",
+                newName: "Nmae");
+        }
+    }
+}
